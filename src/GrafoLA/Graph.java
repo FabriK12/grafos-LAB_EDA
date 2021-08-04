7fe192d41e64e8584234155d6b88ff5104294e4e
FabriK12 <fbalarezo@unsa.edu.pe>
2021-08-03 20:40:45 -0500

    Escribiendo constructores y metodos de la clase Edge

--- src/GrafoLA/Graph.java
+++ src/GrafoLA/Graph.java
@@ -4,14 +4,39 @@ import java.util.LinkedList;
 
 public class Graph<E> {
 
-    private class Edge {
+    private class Edge<T> {
         protected Vertex refDestination;
         protected int weight;
 
+        public Edge(Vertex refDestination) {
+            this(refDestination, -1);
+        }
+
+        public Edge(Vertex refDestination, int weight) {
+            this.refDestination = refDestination;
+            this.weight = weight;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(obj instanceof Edge<?>) {
+                Edge<T> e = (Edge<T>) obj;
+                return this.refDestination.equals(e.refDestination);
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            if(this.weight > -1)
+                return refDestination.data + " {" + this.weight + "}, ";
+            else
+                return refDestination.data + ", ";
+        }
     }
     private class Vertex {
         protected E data;
-        protected LinkedList<Edge> adjacencyList;
+        protected LinkedList<Edge<E>> adjacencyList;
     }
 
     protected LinkedList<Vertex> listVertices;
