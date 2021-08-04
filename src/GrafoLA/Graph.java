63938107b9d99745d5b2c611aad090062485249b
FabriK12 <fbalarezo@unsa.edu.pe>
2021-08-03 20:46:58 -0500

    Escribiendo constructores y metodos de la clase Vertex

--- src/GrafoLA/Graph.java
+++ src/GrafoLA/Graph.java
@@ -5,14 +5,14 @@ import java.util.LinkedList;
 public class Graph<E> {
 
     private class Edge<T> {
-        protected Vertex refDestination;
+        protected Vertex<T> refDestination;
         protected int weight;
 
-        public Edge(Vertex refDestination) {
+        public Edge(Vertex<T> refDestination) {
             this(refDestination, -1);
         }
 
-        public Edge(Vertex refDestination, int weight) {
+        public Edge(Vertex<T> refDestination, int weight) {
             this.refDestination = refDestination;
             this.weight = weight;
         }
@@ -34,14 +34,33 @@ public class Graph<E> {
                 return refDestination.data + ", ";
         }
     }
-    private class Vertex {
-        protected E data;
-        protected LinkedList<Edge<E>> adjacencyList;
+    private class Vertex<T> {
+        protected T data;
+        protected LinkedList<Edge<T>> adjacencyList;
+
+        public Vertex(T data) {
+            this.data = data;
+            adjacencyList = new LinkedList<Edge<T>>();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(obj instanceof Vertex<?>) {
+                Vertex<T> e = (Vertex<T>) obj;
+                return this.data.equals(e.data);
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return this.data + " -> " + this.adjacencyList.toString() + "\n";
+        }
     }
 
-    protected LinkedList<Vertex> listVertices;
+    protected LinkedList<Vertex<E>> listVertices;
 
     public Graph() {
-        listVertices = new LinkedList<Vertex>();
+        listVertices = new LinkedList<Vertex<E>>();
     }
 }
